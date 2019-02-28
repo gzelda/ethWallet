4dd67b50eb0e2a755b5f2f4af8615e321eaffc3c
tygavin <tianyao@zju.edu.cn>
2019-02-28 19:27:20 +0800

    添加web3接口，个人签名

--- routes/web3/signPersonalMessage.js
+++ routes/web3/signPersonalMessage.js
@@ -13,25 +13,34 @@ var chainConfig = require('../utils/config.js')
 router.post('/', function(req, resp, next) {
 
 	var UID = req.body.UID;
+	var Msg = req.body.Message;
+	console.log(Msg,typeof(Msg));
+	
+
+	if (typeof Msg == "string"){
+		Msg = JSON.parse(Msg);
+		console.log(Msg,typeof(Msg));
+	}
+
+	var data = Msg.data;
+
+	console.log(Msg.data);
 	// https://kovan.infura.io
 	// https://mainnet.infura.io
 	// 0x47B9Be7A0FC74Be3fccdECfC6d41d21D24D4a672
 	// c7a933bf8981d80ea212bb59d9c5af3a8295a2216e94eadc3649e09c94824897
-	var personalData = "0xe6aca2e8bf8ee69da5e588b0e4ba91e69697e9be99efbc81"
+	var personalData = Msg.data
 	var userAddress = "0x47B9Be7A0FC74Be3fccdECfC6d41d21D24D4a672"
 	var userPri = "c7a933bf8981d80ea212bb59d9c5af3a8295a2216e94eadc3649e09c94824897";
 	console.log("in gty:",chainConfig.chainServer);
 	web3 = new Web3(new Web3.providers.HttpProvider(chainConfig.chainServer));
-	web3.eth.getBalance(userAddress).then(console.log);
-	
-	web3.eth.personal.sign(personalData,userAddress,userPri
-		).then(console.log);
+	console.log(web3.version)
 
-
-		
 	
-	
-	
+	var signData = web3.eth.accounts.sign(personalData, userPri).signature;
+	var result = {result:signData}
+	var respData = respJson.generateJson(1,0,"请求成功",result);
+	resp.send(respData);
 });
 
 module.exports = router;
